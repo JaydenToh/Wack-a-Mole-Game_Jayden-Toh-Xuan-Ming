a7abebdb8598e306fb7b8b024114f96a20bd82eb
JaydenToh <s10241868@connect.np.edu.sg>
2026-01-31 00:09:33 +0800

    Working on Game Screen Function. Setting how the data will be save when the app closes so the user can keep track of highs core.
    Setting up the variables for the UI.

--- app/src/main/java/com/example/wackamole/MainActivity.kt
+++ app/src/main/java/com/example/wackamole/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.wackamole
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.wackamole.ui.theme.WackAMoleTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,3 +58,22 @@ fun WhackAMoleApp() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GameScreen(onNavigateToHighScore: () -> Unit) {
+    // Saving the high score data even when app closes
+    val context = LocalContext.current
+    val SharedPreferences= remember {
+        context.getSharedPreferences("score_save", Context.MODE_PRIVATE)
+    }
+
+    var score by remember { mutableStateOf(0) }
+    var timeLeft by remember { mutableStateOf(35) }
+    var currentMoleIndex by remember {mutableStateOf(-1) }
+    var isPlaying by remember { mutableStateOf(false) }
+
+    var highScore by remember {
+        mutableStateOf(SharedPreferences.getInt("high_score",0))
+    }
+
+}

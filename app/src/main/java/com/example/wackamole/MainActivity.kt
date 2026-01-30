d4e8ea48bbc4fcc34d2316f464fa10ff3fe293c1
JaydenToh <s10241868@connect.np.edu.sg>
2026-01-31 00:27:20 +0800

    Working on Game Screen Function. Adding the logic of how high score is updated and setting a simple timer to count down for the game.

--- app/src/main/java/com/example/wackamole/MainActivity.kt
+++ app/src/main/java/com/example/wackamole/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +26,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import kotlinx.coroutines.delay
 import com.example.wackamole.ui.theme.WackAMoleTheme
 
 class MainActivity : ComponentActivity() {
@@ -68,12 +70,31 @@ fun GameScreen(onNavigateToHighScore: () -> Unit) {
     }
 
     var score by remember { mutableStateOf(0) }
-    var timeLeft by remember { mutableStateOf(35) }
+    var timeLeft by remember { mutableStateOf(30) }
     var currentMoleIndex by remember {mutableStateOf(-1) }
     var isPlaying by remember { mutableStateOf(false) }
 
     var highScore by remember {
         mutableStateOf(SharedPreferences.getInt("high_score",0))
     }
+
+    LaunchedEffect(isPlaying) {
+        score = 0
+        timeLeft = 30
+
+        while (timeLeft > 0) {
+            delay(1000)
+            timeLeft = timeLeft - 1
+        }
+
+        isPlaying = false
+        currentMoleIndex = -1
+
+        if (score > highScore) {
+            highScore = score
+            SharedPreferences.edit().putInt("high_score", highScore).apply()
+        }
+    }
+
 
 }
